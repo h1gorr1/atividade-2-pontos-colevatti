f26abd315597af553532b3271228a5ed6e46ee20
h1gorr1 <165603294+h1gorr1@users.noreply.github.com>
2025-11-18 21:16:59 -0300

    Refactor Fibonacci algorithm and improve output messages

--- ex.colevatti.s
+++ ex.colevatti.s
@@ -1,33 +1,32 @@
 #63. Criar um algoritmo que imprima a soma de n primeiros termos da série de fibonacci (2 < n < 40 e, caso não seja, deve ser novamente solicitado).
 .data
 	n: .asciiz "Digite O Valor De N: "
-	total: .asciiz "O Total é :  "
-	lembrete: .asciiz "  (Sequência De Fibonacci Lembrando que a sequencia começa a partir de 3)"
+	total: .asciiz "O Total é "
+	lembrete: .asciiz  " Da Sequência de fibonacci!  "
 	invalido: .asciiz "Valor Inválido, Tente Novamente! "
-	somando: .asciiz "Somando o final de cada Sequencia  o total fica: "
+	somando: .asciiz "Somando o final de cada Sequência  o total fica: "
 	linha: .asciiz "\n"
 	espaco: .asciiz " + "
 	igual: .asciiz " = "
 .text
 main:
-	li $v0,4 #Imprimir String:
+	li $v0,4 
 	la $a0,n
 	syscall
 	
-	# N Valor
-	li $v0,5 #Ler Inteiro
+	li $v0,5 
 	syscall 
 	move $t0,$v0
 	
 	li $t1,2
 	li $t2,40
 	
-	verificar_n:
-	ble $t0,$t1,valor_invalido #Se Menor Ou Igual A 2, Vá Para Valor Inválido.
-	bge $t0,$t2,valor_invalido #Se Maior Ou Igual a 40,vá também para valor inválido.
+verificar_n:
+	ble $t0,$t1,valor_invalido
+	bge $t0,$t2,valor_invalido
 	j calculo_fibonacci
 	
-	valor_invalido:
+valor_invalido:
 	li $v0,4
 	la $a0,invalido
 	syscall
@@ -37,20 +36,34 @@ main:
 	move $t0,$v0
 	j verificar_n
 	
-	calculo_fibonacci:
-	li $t5,0 #Acumulador 0
-	li $t3,1 #Acumulador 1
-	li $t4,1 #Acumulador 2
-	li $t5,0 #Acumulador 3
-	li $t6,3 #Contador a partir do 3
-	li $t7,1 #Incrementar 1
-	li $t8,0 #acumulador de resultados para printar soma
+calculo_fibonacci:
+	li $t3,0      # termo anterior
+	li $t4,1      # termo atual
+	li $t6,2      # contador inicia em 2 já contando 0 e 1
+	li $t7,1      # incremento
+	li $t8,1      # soma inicial = 0 + 1 = 1
 	
+	# imprime 0
+	li $v0,1
+	move $a0,$t3
+	syscall
+	li $v0,4
+	la $a0,linha
+	syscall
 	
-	add_fibonacci:
-	add $t5,$t3,$t4 #1 + 1 = 2
-	add $t8,$t5,$t8 #Somar Os Resultados
+	# imprime 1
+	li $v0,1
+	move $a0,$t4
+	syscall
+	li $v0,4
+	la $a0,linha
+	syscall
 	
+add_fibonacci:
+	add $t5,$t3,$t4   # próximo termo
+	add $t8,$t8,$t5   # soma dos n termos
+	
+	# imprime operação
 	li $v0,1
 	move $a0,$t3
 	syscall
@@ -74,21 +87,16 @@ main:
 	li $v0,4
 	la $a0,linha
 	syscall
-	move $t3,$t4 #Trocar 1 por 1 
-	move $t4,$t5 #trocar 2 por 1
 	
+	# avança
+	move $t3,$t4
+	move $t4,$t5
 	
-	
-	bne $t6,$t0,prossiga #Se Contador For Diferente De n, prossiga
-	j fim
-	
-	prossiga:
+	bge $t6,$t0,fim   # PARA quando t6 == N
 	add $t6,$t6,$t7
 	j add_fibonacci
 	
-	
-	
-	fim:
+fim:
 	li $v0,4
 	la $a0,total
 	syscall
@@ -115,6 +123,7 @@ main:
 	
 	li $v0,10
 	syscall
+
 	
 	
 	
